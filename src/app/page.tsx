1a440fc9686c48601645607c888bd5255d26830f
Choi Chan Hwan <africakokiri@gmail.com>
2025-01-17 03:35:53 +0900

    컴포넌트 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,34 +12,46 @@ const sortedByDatePosts = posts.sort((a, b) => {
   return 0;
 });
 
+const Header = () => {
+  return (
+    <header className="relative flex flex-col items-center gap-8">
+      <CloudImage />
+      <Darkmode />
+    </header>
+  );
+};
+
+const PostsList = () => {
+  return (
+    <ul className="space-y-8">
+      {sortedByDatePosts.map(({ title, slug, date }) => {
+        return (
+          <li
+            key={slug}
+            className="rounded-lg bg-neutral-100 p-2 dark:bg-[#303030]"
+          >
+            <Link
+              href={slug}
+              className="space-y-4"
+            >
+              <p className="text-lg">{title}</p>
+              <p className="flex justify-between text-sm">
+                <span>{slug.split("/")[1]}</span>
+                <span>{date}</span>
+              </p>
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
 export default function MainPage() {
   return (
     <div className="space-y-8 px-4 pb-12 pt-4">
-      <header className="relative flex flex-col items-center gap-8">
-        <CloudImage />
-        <Darkmode />
-      </header>
-      <ul className="space-y-8">
-        {sortedByDatePosts.map(({ title, slug, date }) => {
-          return (
-            <li
-              key={slug}
-              className="rounded-lg bg-neutral-100 p-2 dark:bg-[#303030]"
-            >
-              <Link
-                href={slug}
-                className="space-y-4"
-              >
-                <p className="text-lg">{title}</p>
-                <p className="flex justify-between text-sm">
-                  <span>{slug.split("/")[1]}</span>
-                  <span>{date}</span>
-                </p>
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
+      <Header />
+      <PostsList />
     </div>
   );
 }
