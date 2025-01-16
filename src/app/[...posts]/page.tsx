273226f35d648d74e4173b147a72103ee2c65b67
Choi Chan Hwan <africakokiri@gmail.com>
2025-01-17 05:02:48 +0900

    PostDetailPage nav 구성

--- src/app/[...posts]/page.tsx
+++ src/app/[...posts]/page.tsx
@@ -1,3 +1,4 @@
+import { Darkmode } from "@/components/darkmode-switch";
 import { MDXContent } from "@/components/mdx-content";
 
 import { posts } from "../../../.velite";
@@ -7,11 +8,21 @@ export default async function PostDetailPage({
 }: {
   params: Promise<{ slug: string }>;
 }) {
-  console.log(await params);
-
   return (
-    <article>
-      <MDXContent code={posts[0].code} />
-    </article>
+    <div>
+      <nav
+        className="fixed flex w-full items-center justify-between
+border-b-[1px] border-black bg-white p-4 dark:border-white dark:bg-black"
+      >
+        <h1 className="text-xl">구름</h1>
+        <Darkmode />
+      </nav>
+      <article className="max-w-[768px] overflow-x-hidden px-4 py-20">
+        <div>
+          <h1 className="text-2xl font-bold">{posts[0].title}</h1>
+        </div>
+        <MDXContent code={posts[0].code} />
+      </article>
+    </div>
   );
 }
